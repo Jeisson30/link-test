2504824b7d90fccc4c1d0eda796622f9b0d673b6
Jeisson <jeda1989@gmail.com>
2025-04-06 00:54:33 -0500

    Init proyect

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div class="min-h-screen bg-slate-100 flex items-center justify-center">
+    <h1 class="text-3xl font-bold text-blue-600">Init</h1>
+  </div>
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+  </div>
+</template>
